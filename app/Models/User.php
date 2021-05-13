762396c405367d02eb07e0cbaf37e760e886ea81
Francisco José Gordo Salguero <franciscojgsspam@gmail.com>
2021-05-13 12:17:37 +0200

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,15 +8,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable {
-
+class User extends Authenticatable
+{
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
-    use HasTeams;
     use Notifiable;
     use TwoFactorAuthenticatable;
 
@@ -26,9 +24,10 @@ class User extends Authenticatable {
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
-
 
     /**
      * The attributes that should be hidden for arrays.
@@ -59,9 +58,4 @@ class User extends Authenticatable {
     protected $appends = [
         'profile_photo_url',
     ];
-
-    public function comunidades() {
-        return $this->belongsToMany(Comunidad::class, 'comunidad_user','user_id','comunidad_id')->withTimestamps();
-    }
-
 }
