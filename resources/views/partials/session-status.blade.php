530c7cbcd30821d946375d1b12772c3780f3688b
Francisco José Gordo Salguero <franciscojgsspam@gmail.com>
2021-05-23 19:53:19 +0200

    cambiando session-status

--- resources/views/partials/session-status.blade.php
+++ resources/views/partials/session-status.blade.php
@@ -1,8 +1,8 @@
 @if (session('status'))
-<div class="alert {{ session('status')[1] }} alert-dismissible fade show" role="alert">
-    {{ session('status')[0] }}
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
+    <div class="alert {{ session('status')[1] }} alert-dismissible fade show" role="alert">
+        {{ session('status')[0] }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
 @endif
