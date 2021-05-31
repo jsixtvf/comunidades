3564599133b4e9cf352772d80b3e349cb267341d
Francisco José Gordo Salguero <franciscojgsspam@gmail.com>
2021-05-31 18:23:47 +0200

    Archivo hecho por Alex Gonzalez que me faltaba para que funcionase su parte

--- database/migrations/2021_04_07_172251_create_propiedades_table.php
+++ database/migrations/2021_04_07_172251_create_propiedades_table.php
@@ -15,6 +15,8 @@ class CreatePropiedadesTable extends Migration
     {
         // original Randion
         Schema::create('propiedades', function (Blueprint $table) {
+            // Original de Randion
+            /*
             $table->id();
             $table->timestamps();
             $table->softDeletes();
@@ -22,6 +24,31 @@ class CreatePropiedadesTable extends Migration
             $table->unsignedBigInteger('users_id')->nullable();  // Solo consideramos un propietario por propiedad
             
             $table->integer('parte')->comment("Cada una de las partes que componen la comunidad, según registro de la propiedad");
+            $table->integer('coeficiente')->comment("Porcentage de participación en el total de la comunidad, según registro de la propiedad");
+
+          // posiblemente se enlace con una entidad 'tipo_propiedad' para controlar el dominio de valores
+            $table->enum('tipo',['local','piso','atico'])->comment("Tipo de propiedad: piso, ático, local,...");
+            $table->string('observaciones')->nullable();
+            
+
+            $table->foreign('users_id')->references('id')->on('users');
+            
+            $table->foreign('comunidades_id')->references('id')->on('comunidades')
+                    ->onDelete('cascade');
+            
+            $table->index(['comunidades_id','parte']);
+             */
+            
+            // Parte de Rafa Gonzalez
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('comunidades_id')->nullable();
+            $table->string('nombre');
+            $table->string('propietario');
+            $table->unsignedBigInteger('users_id')->nullable();  // Solo consideramos un propietario por propiedad
+            
+            $table->string('parte')->comment("Cada una de las partes que componen la comunidad, según registro de la propiedad");
             $table->integer('coeficiente')->comment("Porcentage de participación en el total de la comunidad, según registro de la propiedad");
 
           // posiblemente se enlace con una entidad 'tipo_propiedad' para controlar el dominio de valores
